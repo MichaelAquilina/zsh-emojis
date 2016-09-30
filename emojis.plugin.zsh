09739f18f0f0687831ffb14adbac46abca30c8a2
Michael Aquilina <michaelaquilina@gmail.com>
2016-09-30 17:28:22 +0100

    Add hashbang and sort alphabetically

--- emojis.plugin.zsh
+++ emojis.plugin.zsh
@@ -1,7 +1,9 @@
+#! /bin/zsh
+
 # contains numerous variables for common ascii emojis
 
-export shrug='¯\_(ツ)_/¯'
 export fliptablei='(╯°□°）╯︵ ┻━┻'
 export lennyface='( ͡° ͜ʖ ͡°)'
 export muscleflex='ᕙ(⇀‸↼‶)ᕗ'
 export piggy='( ́・ω・`)'
+export shrug='¯\_(ツ)_/¯'
