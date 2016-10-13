ff21feb737c3ee487ab2f39baf9c156c535e22bd
Michael Aquilina <michaelaquilina@gmail.com>
2016-10-13 18:09:23 +0100

    Add a lot more emojis

--- emojis.plugin.zsh
+++ emojis.plugin.zsh
@@ -2,8 +2,18 @@
 
 # contains numerous variables for common ascii emojis
 
+export confused='¯\_(⊙︿⊙)_/¯'
+export crying='ಥ_ಥ'
+export cute_bear='ʕ•ᴥ•ʔ'
+export cute_face='(｡◕‿◕｡)'
+export fisticuffs='ლ(｀ー´ლ)'
 export fliptable='(╯°□°）╯︵ ┻━┻'
+export happy='ヽ(´▽`)/'
+export innocent='ʘ‿ʘ'
+export kirby='⊂(◉‿◉)つ'
 export lennyface='( ͡° ͜ʖ ͡°)'
+export lion='°‿‿°'
 export muscleflex='ᕙ(⇀‸↼‶)ᕗ'
+export perky='(`・ω・´)'
 export piggy='( ́・ω・`)'
 export shrug='¯\_(ツ)_/¯'
